1b1c0de8dbdc7509ee670c1b3118d349b173a8b1
Alan Secundo <alan_secundo@hotmail.com>
2023-08-08 22:53:03 -0300

    feat: remoção de parte de blogs, customização inicial da home

--- docs/intro.md
+++ docs/intro.md
@@ -2,7 +2,7 @@
 sidebar_position: 1
 ---
 
-# Tutorial Intro
+# DB1 Way
 
 Let's discover **Docusaurus in less than 5 minutes**.
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,35 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import styles from "./index.module.css";
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      description="Description will go into a meta tag in <head />"
+    >
       <main>
-        <HomepageFeatures />
+        <div className={styles.homePage}>
+          <div className={styles.content}>
+            <div className={styles.introTitle}>
+              Product
+              <br />
+              Guide
+            </div>
+            <div className={styles.textContainer}>
+              <div className={styles.line}></div>
+              <div className={styles.subText}>
+                Lorem ipsum dolor sit amet. Cum voluptatum incidunt ad pariatur
+                vitae et fugiat pariatur et rerum voluptatem et tempore fuga.
+                Vel commodi eius ea assumenda quibusdam vel quia corrupti eum
+                officia voluptas ut dolores velit.
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </Layout>
   );
